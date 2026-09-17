01584e5f5247a3bd82bb3fd7aeab0a267ed69eec
Manojmi192 <n.manojmj1997@gmail.com>
2026-09-17 15:36:49 +0530

    first commit

---
+++ Task2.js
@@ -0,0 +1,8 @@
+let name="Fazil";
+let programmingLanguage="JavaScript";
+let Exp=2;
+
+console.log("My name is",name,"and i am learning ",programmingLanguage,". I have",Exp,"years of experience in programming.");
+
+Exp=3;
+console.log("Next Year",Exp);
---
+++ Variables.js
@@ -0,0 +1,9 @@
+let name = "Fazil";
+var age = 24;
+const native = "India";
+
+console.log("my name is",name);
+console.log("My age is ",age);
+console.log("I am from",native); 
+
+
